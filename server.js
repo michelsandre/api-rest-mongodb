4d3a0ae59d9e8654bb9d9b6ef1f6c2fb367dc61e
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-12-10 20:46:56 -0300

    removed confidential info

--- server.js
+++ server.js
@@ -6,6 +6,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Servidor escutando em http://localhost:3000");
 });
-
-//env file
-//mongodb+srv://admin:<pwd>@cluster0.xkz6rva.mongodb.net/livraria?retryWrites=true&w=majority
