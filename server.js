84a19dbb64b0ad2f3dfcf1ba48225f39b8cdd8bc
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-12-10 20:35:23 -0300

    inserido mongodb

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
@@ -5,3 +6,6 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Servidor escutando em http://localhost:3000");
 });
+
+//env file
+//mongodb+srv://admin:<pwd>@cluster0.xkz6rva.mongodb.net/livraria?retryWrites=true&w=majority
--- src/app.js
+++ src/app.js
@@ -1,56 +1,19 @@
 import express from "express";
-import fs from "fs";
+import conectaDB from "./config/dbConnect.js";
+import routes from "./config/routes/index.js";
 
-const file = fs.readFileSync("./src/livros.json");
-const listaLivros = JSON.parse(file);
+const conexao = await conectaDB();
+
+conexao.on("error", (erro) => {
+  console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com banco realizado com sucesso");
+});
 
 const app = express();
-app.use(express.json());
-
-// const livros = [
-//   {
-//     id: 1,
-//     titulo: "O senhor dos aneis",
-//   },
-//   {
-//     id: 2,
-//     titulo: "O senhor da guerra",
-//   },
-// ];
-
-function buscaLivros(id) {
-  return livros.findIndex((livro) => {
-    return livro.id === Number(id);
-  });
-}
-
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Nodejs");
-});
-
-app.get("/livros", (req, res) => {
-  res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivros(req.params.id);
-  res.status(200).json(listaLivros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  //   livros.push(req.body);
-  listaLivros.push(req.body);
-  fs.writeFileSync("./src/livros.json", JSON.stringify(listaLivros));
-
-  res.status(201).send("Livro cadastrado");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivros(req.params.id);
-  listaLivros[index].titulo = req.body.titulo;
-  fs.writeFileSync("./src/livros.json", JSON.stringify(listaLivros));
-  res.status(200).json(listaLivros);
-});
+routes(app);
 
 app.delete("/livros/:id", (req, res) => {
   const index = buscaLivros(req.params.id);
