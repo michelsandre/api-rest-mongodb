4f8da38ed39b8cbf8fb1430c18a00c6f0f4eab40
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-12-13 08:58:55 -0300

    add vercel json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor escutando em http://localhost:3000");
+  console.log(`Server listening on locahost:${PORT}`);
 });
