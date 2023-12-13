24193a96140c87459a85e68cdcce048708f3b705
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-12-13 09:03:37 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectaDB from "./config/dbConnect.js";
 import routes from "./config/routes/index.js";
+import "dotenv/config";
 
 const conexao = await conectaDB();
 
@@ -15,4 +16,11 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-export default app;
+//Novo implementacao
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on locahost:${PORT}`);
+});
+
+// export default app;
